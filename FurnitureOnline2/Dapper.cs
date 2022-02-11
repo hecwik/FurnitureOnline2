f8ef8e3e4f1e4bbc0b92bfd19f52a9ef033bf59a
Hector Wiklund <heckee@gmail.com>
2022-02-11 17:38:43 +0100

    Added classes and etc

--- FurnitureOnline2/Dapper.cs
+++ FurnitureOnline2/Dapper.cs
@@ -10,18 +10,50 @@ namespace FurnitureOnline2
 {
     class Dapper
     {
-        // static string connString = "data source=dbwebshop1.database.windows.net; initial catalog = WebShopDB; persist security info = True; Integrated Security = True;";
         static string connString = "Server=dbwebshop1.database.windows.net;Database=WebShopDB;User Id=Ladmin; Password=L'sdbwebshop1; Encrypt=True; TrustServerCertificate=False";
 
-        public static List<Models.Category> GetAllSpots()
+        public static string CustomerOrderList( int customer)
         {
-            var sql = @"SELECT * FROM Category";
 
-            var spotsPerHouse = new List<Models.Category>();
+            string returnString = "";
+            var sql = @"Select Id, OrderDate, TotalPrice  
+                        FROM OrderHistory
+                        Where CustomerId = " + customer;
+
             using (var connection = new SqlConnection(connString))
             {
                 connection.Open();
-                spotsPerHouse = connection.Query<Models.Category>(sql).ToList();
+                var orderlist = connection.Query<Models.OrderHistory>(sql).ToList();
+
+                foreach (var item  in orderlist)
+                {
+                    returnString += $"{item.Id}  {item.OrderDate}  {item.TotalPrice}";
+                }
+
+            }
+            return returnString;
+        }
+
+        public static string ShowSpecificOrder(int orderId)
+        {
+
+            string returnString = "";
+            var sql = @"Select * FROM OrderHistory oh
+                        Join OrderDetail od ON od.OrderId = oh.Id
+                        Join Products p ON od.ProductsId = p.ArticleNumber
+                        Join Payment pa ON oh.PaymentId = pa.id
+                        Join Shipping s ON oh.ShippingId = s.Id
+                        Where oh.id = 0";
+
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                var product = connection.Query<Models.OrderHistory>(sql).ToList();
+
+                foreach (var item in product)
+                {
+                    returnString += $"{ item.Id}  {item.OrderDate}  {item.TotalPrice}"
+                }
 
             }
             return spotsPerHouse;
---
+++ FurnitureOnline2/ShowSpecificOrderQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureOnline2
+{
+    class ShowSpecificOrderQuery
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float? TotalAmount { get; set; }
+        public string ShippingAdress { get; set; }
+        public string ShippingZipCode { get; set; }
+        public string ShippingCity { get; set; }
+        public int? ArticleNr { get; set; }
+        public string ArticleName { get; set; }
+        public int? Quantity { get; set; }
+        public float? ProductPris { get; set; }
+        public string ShippingMethod { get; set; }
+
+
+
+
+    }
+}

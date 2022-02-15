176b6b7da8b55f492ff5ff38ffd8d13050d750d6
Lollo <Lollo@MSI>
2022-02-15 19:56:11 +0100

    radbrytning per sträng istället i Dapper rad 97

--- FurnitureOnline2/Dapper.cs
+++ FurnitureOnline2/Dapper.cs
@@ -94,7 +94,7 @@ namespace FurnitureOnline2
 
                 foreach (var item in product)
                 {
-                    returnString += $"{item.Item1,-20}\n{item.Item2,-10}{item.Item3,-10}";
+                    returnString += $"{item.Item1,-20}\n{item.Item2,-10}{item.Item3,-10}\n";
                 }
                 Console.WriteLine(returnString);
                 return returnString;
